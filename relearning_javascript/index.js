4bcde505a204dc94aa91a61818374fb62631bad8
adoolaTurayk <abdallahforyt4@gmail.com>
2026-03-17 16:11:23 +0300

    Update index.js

--- relearning_javascript/index.js
+++ relearning_javascript/index.js
@@ -12,34 +12,34 @@ setInterval(() => {
 
     switch(randNum){
         case 1:
-            t.title = "Dang bro"
+            t.title = "dnkfhbsajkhegdjhwneiouhkiyhjuki"
             break
         case 2:
             t.title = "i heard that shit bruh"
             break
         case 3:
-            t.title = "labubu dubai chocolate matcha"
+            t.title = "switching up on my day1s i js got that 7k check from big yahu"
             break
         case 4:
-            t.title = "Dih"
+            t.title = "killer queen has already touched this tab"
             break
         case 5:
-            t.title = "welcome hello"
+            t.title = "son im crine😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭😭"
             break
         case 6:
-            t.title = "Free robux 🤑🤑🤑🤑"
+            t.title = "never underestimate the power of muhammed awais"
             break
         case 7:
             t.title = "(randNum == 7) = true"
             break
         case 8:
-            t.title = "Mango Mustard 😈🥭🥭"
+            t.title = "rigorous attention to detail"
             break
         case 9:
             t.title = "t.title = 't.title = 't.title = ...'' "
             break
         case 10:
-            t.title = "Do you copy"
+            t.title = "no vibes here"
             break
         default:
             t.title = "error"
